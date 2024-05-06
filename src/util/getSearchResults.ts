a40aedc202ac01aa629efea779a1525b1cb7672f
Nikita Mikheitsev <welt992@gmail.com>
2024-05-06 12:00:05 +0300

    feat: get searh results using api

---
+++ src/util/getSearchResults.ts
@@ -0,0 +1,9 @@
+export async function getRawSearchResults(query: string) {
+  const urlSearch = new URL("https://www.googleapis.com/books/v1/volumes");
+  const params = urlSearch.searchParams;
+  params.set("q", `${query}`);
+  params.set("key", `${import.meta.env.VITE_GOOGLE_API_KEY}`);
+  const response = await fetch(urlSearch);
+  const data = await response.json();
+  return data;
+}
