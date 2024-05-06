f0f517852c10bdf2eb92a69d4ebe529fd608c119
Nikita Mikheitsev <welt992@gmail.com>
2024-05-06 15:02:13 +0300

    fix: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import "./App.css";
 import { SearchBookCards } from "./components/SearchBooksCard";
-import { useState } from "react";
 
 function App() {
-  const [checked, setChecked] = useState(false);
   return <SearchBookCards searchQuery="Lord of the rings"></SearchBookCards>;
   return;
 }
