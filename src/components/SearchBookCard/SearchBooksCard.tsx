114b64496c582a32d998193c3eda91facc8ac2e0
Nikita Mikheitsev <welt992@gmail.com>
2024-05-16 22:56:54 +0300

    refactor: face component

--- src/components/SearchBookCard/SearchBooksCard.tsx
+++ src/components/SearchBookCard/SearchBooksCard.tsx
@@ -1,35 +1,45 @@
-import { useState, useEffect } from "react";
-import { BookData, rawBookData } from "../../entities/BookData";
-import { getRawSearchResults } from "../../util/getSearchResults";
+import { getSearchResultsString } from "../../util/getSearchResults";
 import { BookCard } from "../BookCard/BookCard";
 import "./SearchBooksCard.css";
 import SearchBar from "../SearchBar/SearchBar";
+import { useGetVolumesQuery } from "../../App/apiSlice";
+import { checkIfFavorite } from "../../util/checkIfFavorite";
 
 type SearchBookCardsProps = { searchQuery: string };
 
 export function SearchBookCards({ searchQuery }: SearchBookCardsProps) {
-  const [loading, setLoading] = useState(true);
-  const [resultList, setResultList] = useState<BookData[]>([]);
-  const query = searchQuery === "" ? "Dune" : searchQuery;
-  useEffect(() => {
-    getRawSearchResults(query).then((response) => {
-      setResultList(() =>
-        response.items.map((item: rawBookData) => new BookData(item)),
-      );
-      setLoading(() => false);
-    });
-  }, [query]);
-
-  return loading ? (
-    <div>Loading...</div>
-  ) : (
-    <>
-      <SearchBar currentSearch="" />
-      <ul className="book-cards">
-        {resultList.map((item) => (
-          <BookCard item={item} key={item.id}></BookCard>
-        ))}
-      </ul>
-    </>
+  const { data, isFetching, isSuccess } = useGetVolumesQuery(
+    getSearchResultsString(searchQuery, "10"),
   );
+
+  let searchResults;
+  if (isFetching) {
+    searchResults = (
+      <div className="quick-search border list-group p-1">Загружаем...</div>
+    );
+  } else if (isSuccess) {
+    searchResults = (
+      <>
+        <SearchBar currentSearch="" />
+
+        <ul className="book-cards">
+          {!data ? (
+            <div>Ничего не было найдено</div>
+          ) : (
+            data.map((item) => (
+              <BookCard
+                item={{
+                  ...item,
+                  isInFavorites: checkIfFavorite(item.id),
+                }}
+                key={item.id}
+              ></BookCard>
+            ))
+          )}
+        </ul>
+      </>
+    );
+  }
+
+  return <>{searchResults}</>;
 }
